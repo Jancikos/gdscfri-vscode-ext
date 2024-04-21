4645b5b6426d5611f98d6a66e442460a10bc4c4c
Ján Kostor <ko.janci@gmail.com>
2024-04-21 12:05:01 +0200

    cmd aus filter data -> multiple filter types

--- src/commands/AusFilterDataCommand.ts
+++ src/commands/AusFilterDataCommand.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { VillageLoader } from '../tools/VillageLoader';
+import { Village } from '../models/Village';
 
 export class AusFilterDataCommand {
 
@@ -19,18 +20,39 @@ export class AusFilterDataCommand {
         const data = villageLoader.loadVillages();
 
         // filter the data
-        const filterQuery = await vscode.window.showInputBox({
-            placeHolder: 'Filter query'
+        const filterTypes = AusFilterDataCommand.getFilterTypes();
+
+        // select filter type
+        const filterTypeTitle = await vscode.window.showQuickPick(
+            filterTypes.map(type => type.title), {
+            placeHolder: 'Select filter type'
         });
 
-        if (!filterQuery) {
+        const filterType = filterTypes.find(type => type.title === filterTypeTitle);
+        if (!filterType) {
+            vscode.window.showErrorMessage('Filter type not selected.');
+            return;
+        }
+
+        const filterQuery =
+            filterType.filterFunction
+                ? await vscode.window.showInputBox({
+                    placeHolder: 'Filter query'
+                })
+                : undefined;
+        if (!filterQuery && filterType.filterFunction) {
             vscode.window.showErrorMessage('Filter query cant be empty.');
             return;
         }
 
         // execute filter
         const filteredData = data
-            .filter(village => village.name.startsWith(filterQuery))
+            .filter(
+                village =>
+                    filterType.filterFunction
+                        ? filterType.filterFunction(village, filterQuery)
+                        : true
+            )
             .sort((a, b) => a.name.localeCompare(b.name));
 
         // output data
@@ -39,6 +61,7 @@ export class AusFilterDataCommand {
         outputContent +=
             `# Villages
 Loaded from \`${pathToCSV}\`.\n
+Filter type \`${filterType.title}\`.\n
 Filter query \`${filterQuery}\`.\n
 Total count \`${filteredData.length}\`.\n
 
@@ -59,8 +82,23 @@ Total count \`${filteredData.length}\`.\n
         await vscode.window.showTextDocument(document);
         // show markdown preview
         await vscode.commands.executeCommand('markdown.showPreview', document.uri);
-
-
     }
 
+    public static getFilterTypes(): FilterType[] {
+        return [
+            new FilterType('Starts with', (village, filterQuery) => village.name.startsWith(filterQuery || '')),
+            new FilterType('Ends with', (village, filterQuery) => village.name.endsWith(filterQuery || '')),
+            new FilterType('Contains', (village, filterQuery) => village.name.includes(filterQuery || '')),
+            new FilterType('None')
+        ];
+    }
+}
+
+class FilterType {
+    title: string;
+    filterFunction?: (village: Village, filterQuery: string | undefined) => boolean;
+    constructor(title: string, filterFunction?: (village: Village, filterQuery: string | undefined) => boolean) {
+        this.title = title;
+        this.filterFunction = filterFunction;
+    }
 }
