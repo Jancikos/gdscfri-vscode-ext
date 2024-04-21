4c43f786dfc8916292a64d9575e66c514cc459d7
Ján Kostor <ko.janci@gmail.com>
2024-04-21 11:03:10 +0200

    model and loader for villages

---
+++ src/models/Village.ts
@@ -0,0 +1,11 @@
+export class Village {
+    code: string;
+    name: string;
+    type: string;
+
+    constructor(code: string, name: string, type: string) {
+        this.code = code;
+        this.name = name;
+        this.type = type;
+    }
+}
