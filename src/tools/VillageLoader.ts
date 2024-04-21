09c1c15a15cb99a7e873a72edaebd15c44d5f183
Ján Kostor <ko.janci@gmail.com>
2024-04-21 11:03:10 +0200

    model and loader for villages

---
+++ src/tools/VillageLoader.ts
@@ -0,0 +1,39 @@
+import * as fs from 'fs';
+import { Village } from "../models/Village";
+
+
+export class VillageLoader {
+    pathToCSV: string;
+
+    constructor(pathToCSV: string) {
+        this.pathToCSV = pathToCSV;
+    }
+
+
+    loadVillages(): Village[] {
+        const fileContent = fs.readFileSync(this.pathToCSV, 'utf8');
+
+        const rows = fileContent.split('\n');
+        
+        const data: Village[] = [];
+        for (let i = 1; i < rows.length; i++) {
+            const row = rows[i];
+            const columns = row.split(';');
+
+            if (columns.length < 18) {
+                continue;
+            }
+
+            const [code, name, type] = columns.slice(4, 7);
+
+            if (code === '' || name === '' || type === '') {
+                continue;
+            }
+
+            const village = new Village(code, name, type);
+            data.push(village);
+        }
+        
+        return data;
+    }
+}
