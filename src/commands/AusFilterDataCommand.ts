c1d5f8513f0f013d3a75935066b347bc6da16579
Ján Kostor <ko.janci@gmail.com>
2024-04-21 11:05:22 +0200

    cmd aus filter data -> loading data from param gdscfri.aus.pathToVillages

--- src/commands/AusFilterDataCommand.ts
+++ src/commands/AusFilterDataCommand.ts
@@ -1,9 +1,26 @@
 import * as vscode from 'vscode';
+import { VillageLoader } from '../tools/VillageLoader';
 
 export class AusFilterDataCommand {
 
     public static async run() {
+        const config = vscode.workspace.getConfiguration();
+
         vscode.window.showInformationMessage('Aus Filter Data command');
+
+        // load the villages
+        const pathToCSV = config.get('gdscfri.aus.pathToVillages'); 
+        if (!pathToCSV) {
+            vscode.window.showErrorMessage('Path to villages is not set. Set it in extension settings.');
+            return;
+        }
+
+        const villageLoader = new VillageLoader(pathToCSV as string);
+
+        const data = villageLoader.loadVillages();
+
+        // show info that the villages are loaded
+        vscode.window.showInformationMessage('Villages are loaded');
     }
 
 }
