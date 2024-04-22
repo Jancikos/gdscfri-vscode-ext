db226b78a733fc9a484ef9ce7a4276135478e41e
Ján Kostor <ko.janci@gmail.com>
2024-04-22 22:00:21 +0200

    getting ready for publish

---
+++ README.md
@@ -0,0 +1,16 @@
+# GDSC FRI extension
+
+This is a GDSC FRI extension. It is a simple extension which was created on the [GDSC FRI workshop](https://gdsc.community.dev/events/details/developer-student-clubs-university-of-zilina-presents-code-with-jan-amp-bots-how-are-they-detected/).
+
+## What does this extension do?
+
+This extension adds command `GDSC FRI: AUS filter data`, which helps students to check correct answers for their semester project from AUS. Before running  this command, you need to have configured path to your AUS data in the settings - param `gdscfri.aus.pathToVillages`. 
+
+## What's next?
+
+This extension is just a simple example of what you can do with VS Code extensions. Feel free to fork [this repository](https://github.com/jancikos/gdscfri-vscode-ext) and create your own extension. You can find more information about creating extensions in the [official documentation](https://code.visualstudio.com/api).
+
+## Authors
+
+- [Jan Kostor](https://github.com/jancikos) - coder
+- [Adam Kocian](https://www.linkedin.com/in/adamkocian) - idea maker
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,7 @@
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { AusFilterDataCommand } from './commands/AusFilterDataCommand';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gdscfri" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('gdscfri.hello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello to GDSC FRI!');
-    });
-    context.subscriptions.push(disposable);
-
-
     const cmdAusFilterData = vscode.commands.registerCommand('gdscfri.aus-filter-data', AusFilterDataCommand.run);
     context.subscriptions.push(cmdAusFilterData);
 }
